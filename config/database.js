6d52850edb0440d2392f1d0a822a82bf95b5bbdd
sreekanth <kothasrikanthreddi@gmail.com>
2017-11-18 00:52:50 +0530

    deployment

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ const crypto = require('crypto').randomBytes(256).toString('hex'); // Provides c
 
 // Export config object
 module.exports = {
-  uri: 'mongodb://localhost:27017/mean-angular-2', // Databse URI and database name
+  uri: 'mongodb://sree:9963447141@ds155674.mlab.com:55674/angular2', // Databse URI and database name
   secret: crypto, // Cryto-created secret
-  db: 'mean-angular-2' // Database name
+  db: 'angular2' // Database name
 }
