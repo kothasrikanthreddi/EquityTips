630ad4178c5fb035ff298cf830f957203874476f
sreekanth <kothasrikanthreddi@gmail.com>
2017-11-25 02:12:25 +0530

    Changed adding dynamic domain name

--- index.js
+++ index.js
@@ -25,6 +25,7 @@ mongoose.connect(config.uri, (err) => {
 // Middleware
 app.use(cors({ origin: 'http://localhost:4200' }));
 app.use(cors({ origin:'http://www.smartequitytips.com'}));
+app.use(cors({ origin:'https://www.smartequitytips.com'}));
 app.use(bodyParser.urlencoded({ extended: false })); // parse application/x-www-form-urlencoded
 app.use(bodyParser.json()); // parse application/json
 app.use(express.static(__dirname + '/public')); // Provide static directory for frontend
